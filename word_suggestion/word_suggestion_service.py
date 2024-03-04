4a0679ad7588a58bc57a8ecc446a6d77cb029a12
Mdperez19 <mdperezpriego@gmail.com>
2024-03-03 20:37:28 -0600

    Remove NLP and NLPBert classes, refactor WordSuggestionService

--- word_suggestion/word_suggestion_service.py
+++ word_suggestion/word_suggestion_service.py
@@ -1,12 +1,34 @@
-from word_suggestion.natural_language_processing_tools.NLPBert import NLPBert
+from word_suggestion.natural_language_processing_tools.text_preprocessing.normalizer.NormalizerBert import \
+    NormalizerBert
+from word_suggestion.natural_language_processing_tools.text_preprocessing.tokenizer.TokenizerNltk import TokenizerNltk
+from word_suggestion.natural_language_processing_tools.text_processor.word_suggester.WordSuggesterBert import \
+    WordSuggesterBert
 
 
 class WordSuggestionService:
     def __init__(self):
-        self.nlp = NLPBert()
+        self.tokenizer = TokenizerNltk()
+        self.normalizer = NormalizerBert()
+        self.word_suggester = WordSuggesterBert()
 
     def suggest_next_word(self, sentence: str) -> list:
-        tokenized_sentence = self.nlp.tokenize_sentence(sentence)
-        normalized_sentence = self.nlp.normalize_tokens(tokenized_sentence)
-        suggested_words = self.nlp.suggest_next_word(normalized_sentence)
+        normalized_sentence = self.prepare_sentence(sentence)
+        suggested_words = self.word_suggester.suggest_next_word(normalized_sentence)
         return suggested_words
+
+    def prepare_sentence(self, sentence: str) -> str:
+        tokenized_sentence = self.tokenizer.tokenize_sentence_by_words(sentence)
+        normalized_sentence_tokens = self.normalizer.normalize_tokens(tokenized_sentence)
+        normalized_sentence_string = self.recover_sentence_from_tokens(normalized_sentence_tokens)
+        masked_sentence = self.add_mask_to_sentence(normalized_sentence_string)
+        return masked_sentence
+
+    @staticmethod
+    def recover_sentence_from_tokens(tokens: list) -> str:
+        sentence = " ".join(tokens)
+        return sentence
+
+    @staticmethod
+    def add_mask_to_sentence(sentence: str) -> str:
+        sentence_with_mask = sentence + " [MASK]"
+        return sentence_with_mask
