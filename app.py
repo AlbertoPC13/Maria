ec81517d13fbe5ef503291ee5f87f6c92b08961b
Mdperez19 <mdperezpriego@gmail.com>
2024-03-03 18:56:07 -0600

    Add dotenv support to load environment variables

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+from dotenv import load_dotenv
 from flask import Flask
 from word_suggestion.word_suggestion_controller import word_suggestion_api
+import os
 
 app = Flask(__name__)
+
+if os.path.exists('.env'):
+    load_dotenv()
+else:
+    print('.env file not found')
 
 app.register_blueprint(word_suggestion_api, url_prefix='/word_suggestion')
 
