1790e19bef075cba266e1c2d6c915fc974fb28a2
Mdperez19 <mdperezpriego@gmail.com>
2024-03-03 20:13:52 -0600

    Refactor WordSuggesterBert class to improve code readability and maintainability

--- word_suggestion/natural_language_processing_tools/text_processor/word_suggester/WordSuggesterBert.py
+++ word_suggestion/natural_language_processing_tools/text_processor/word_suggester/WordSuggesterBert.py
@@ -10,15 +10,15 @@ class WordSuggesterBert(WordSuggester):
         self.headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
     def suggest_next_word(self, normalized_sentence: str) -> list:
-        suggested_words = self.predict_next_word_with_bert(normalized_sentence)
-        return suggested_words
-
-    def predict_next_word_with_bert(self, sentence: str) -> list:
-        response_from_bert = self.get_predictions_from_bert_api(sentence)
-        suggested_words = [(item['token_str'], item['score']) for item in response_from_bert]
-        return suggested_words
+        response_from_bert = self.get_predictions_from_bert_api(normalized_sentence)
+        suggested_words_with_scores = self.extract_word_scores_from_api_response(response_from_bert)
+        return suggested_words_with_scores
 
     def get_predictions_from_bert_api(self, sentence: str) -> list:
         input_data = {"inputs": sentence}
         response = requests.post(self.API_URL, headers=self.headers, json=input_data)
         return response.json()
+
+    @staticmethod
+    def extract_word_scores_from_api_response(response_from_bert):
+        return [(item['token_str'], item['score']) for item in response_from_bert]
