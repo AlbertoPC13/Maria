1aa0eaae1aaf3889cf94c43959c5fcb7ac1ce106
Mdperez19 <mdperezpriego@gmail.com>
2024-03-30 17:09:53 -0600

    Use mocks to test suggest_next_word method

--- tests/natural_language_processing_tools/text_processor/word_suggester/test_word_suggester.py
+++ tests/natural_language_processing_tools/text_processor/word_suggester/test_word_suggester.py
@@ -1,6 +1,7 @@
 import time
 
 import pytest
+import pytest_mock
 from dotenv import load_dotenv
 
 from word_suggestion.natural_language_processing_tools.text_processor.word_suggester.WordSuggesterBert import \
@@ -21,19 +22,37 @@ class TestWordSuggesterBert:
     # TODO: this suite needs more tests
     sentences = [
         "El bosque es un lugar mágico para las [MASK]",
-        "Cuando alguien aprende a programar frecuentemente comete [MASK] fáciles de solucionar"
+        "Cuando alguien aprende a programar frecuentemente comete [MASK] fáciles de solucionar",
+        "Por favor no me hagan entrar [MASK]",
+        "Trata de no hacer [MASK]"
     ]
 
-    @pytest.mark.parametrize("sentences", sentences)
     @pytest.mark.word_suggester
-    def test_suggest_next_word(self, word_suggester_bert, sentences):
-        # Given sentences
+    def test_suggest_next_word(self, word_suggester_bert, mocker):
+        # Given
+        sentence = "El bosque es un lugar mágico para las [MASK]"
+        mocked_predictions = [{'token_str': 'bosque', 'score': '0.44048866629600525'},
+                              {'token_str': 'infierno', 'score': '0.09329268336296082'},
+                              {'token_str': 'cielo', 'score': '0.07073493301868439'},
+                              {'token_str': 'castillo', 'score': '0.03358548879623413'},
+                              {'token_str': '.', 'score': '0.019639084115624428'}]
+
+        mock_get_predictions_from_bert_api = mocker.patch.object(
+            WordSuggesterBert,
+            "get_predictions_from_bert_api",
+            return_value=mocked_predictions
+        )
+
+        scores_expected = [("bosque", "0.44048866629600525"), ("infierno", "0.09329268336296082"),
+                           ("cielo", "0.07073493301868439"), ("castillo", "0.03358548879623413"),
+                           (".", "0.019639084115624428")]
+
         # When
-        suggested_words_with_scores = word_suggester_bert.suggest_next_word(sentences)
+        suggested_words_with_scores = word_suggester_bert.suggest_next_word(sentence)
+
         # Then
-        assert suggested_words_with_scores is not None
-        assert len(suggested_words_with_scores) > 0
-        assert isinstance(suggested_words_with_scores, list)
+        mock_get_predictions_from_bert_api.assert_called_once_with(sentence)
+        assert suggested_words_with_scores == scores_expected
 
     @pytest.mark.parametrize("sentences", sentences)
     @pytest.mark.word_suggester
@@ -41,9 +60,10 @@ class TestWordSuggesterBert:
         # Given sentences
         # When
         predictions = word_suggester_bert.get_predictions_from_bert_api(sentences)
-        print(predictions)
         # Then
         assert predictions is not None
+        assert len(predictions) > 0
+        assert isinstance(predictions, list)
 
     @pytest.mark.word_suggester
     def test_extract_word_scores_from_api_response(self, word_suggester_bert):
