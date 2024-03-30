ab1d2f216ef29c22a75a4dcd6688d28f56b6ab4c
Mdperez19 <mdperezpriego@gmail.com>
2024-03-30 17:46:11 -0600

    Add test cases for tokenizing sentences by words

--- tests/natural_language_processing_tools/text_preprocessing/tokenizer/test_Tokenizer.py
+++ tests/natural_language_processing_tools/text_preprocessing/tokenizer/test_Tokenizer.py
@@ -17,3 +17,24 @@ class TestTokenizerNltk:
         tokenized_sentence = tokenizer_nltk.tokenize_sentence_by_words(sentence)
         # Then
         assert tokenized_sentence == expected_tokenized_sentence
+
+    sentences = [
+        "El bosque es un lugar mágico para las hadas",
+        "Cuando alguien aprende a programar frecuentemente comete errores fáciles de solucionar",
+        "Por favor no me hagan entrar ahí",
+        "Trata de no hacer ruido",
+    ]
+
+    @pytest.mark.tokenizer
+    @pytest.mark.parametrize("sentence", sentences)
+    def test_tokenize_sentence_by_words(self, tokenizer_nltk, sentence):
+        # Given sentences
+
+        # When
+        tokenized_sentence = tokenizer_nltk.tokenize_sentence_by_words(sentence)
+
+        # Then
+        assert tokenized_sentence is not None
+        assert len(tokenized_sentence) > 0
+        assert isinstance(tokenized_sentence, list)
+        assert all(isinstance(token, str) for token in tokenized_sentence)
